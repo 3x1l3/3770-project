b1255773582d484c7c102e161fb36542fcede3f6
chadklassen1@gmail.com <chadklassen1@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-03-22 01:30:59 +0000

    System Tray Icon Example added

---
+++ system_tray_icon_example/mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+
+
+    QSystemTrayIcon *tray = new QSystemTrayIcon(this);
+    QMenu *context_menu = new QMenu(this);
+    QAction *exit = new QAction("Exit", this);
+    exit->connect(exit, SIGNAL(triggered()), this, SLOT(close()));
+    tray->setIcon(QIcon(QPixmap("shutter.png")));
+    tray->show();
+
+    context_menu->addAction(exit);
+
+    tray->setContextMenu(context_menu);
+
+}
+
+MainWindow::~MainWindow()
+{
+
+}
---
+++ system_tray_icon_example/system_tray_icon_example.pro
@@ -0,0 +1,14 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-03-21T18:48:33
+#
+#-------------------------------------------------
+
+TARGET = system_tray_icon_example
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
