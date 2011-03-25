2a112130826e26f9a735207e06aae2fe85618cf2
peoples1988@gmail.com <peoples1988@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-03-25 04:28:36 +0000

    added widget manager and exmaples

--- trunk/TransparentWindowExample/CustomQWidget.h
+++ trunk/TransparentWindowExample/CustomQWidget.h
@@ -5,54 +5,53 @@
 #include <QMessageBox>
 #include <QtGui>
 
-class CustomQWidget : public QWidget {
+#include <iostream>
+
+#include<QPushButton>
+
+#include "MoveableWidget.h"
+
+#include "WidgetManager.h"
+
+
+class CustomQWidget : public QMainWindow {
 
   Q_OBJECT
 
 public:
 
-  CustomQWidget(QWidget *parent = 0): QWidget(parent) 
+  CustomQWidget(QWidget *parent = 0): QMainWindow(parent) 
   {
+    /////////////////////////////////////////////////////////
+    //THIS IS THE CODE TO INITIALIZE THE WIDGET MANAGER AND USE IT
+    
+    QPushButton *button = new QPushButton("this is a button");
+    QTextEdit * edit = new QTextEdit("this is a text edit");
+    QLabel *label = new QLabel("this is a label");
+    MoveableWidget *move = new MoveableWidget();
+    
+    
+    QPushButton *button2 = new QPushButton("this is a button");
+    QTextEdit * edit2 = new QTextEdit("this is a text edit");
+    QLabel *label2 = new QLabel("this is a label");
+    QWidget *layoutWidget = new QWidget();
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(button2);
+    layout->addWidget(edit2);
+    layout->addWidget(label2);
+    layoutWidget->setLayout(layout);
+    
+    WidgetManager *manager = new WidgetManager();
+    manager->addNewWidget("blah Blah", 0, 0, 0.5, button);
+    manager->addNewWidget("blah Blah", 0, 0, 0.9, edit);
+    manager->addNewWidget("blah Blah", 0, 0, 0.7, label);
+    manager->addNewWidget("blah Blah", 0, 0, 0.1, move);
+    manager->addNewWidget("blah Blah", 0, 0, 0.3, layoutWidget);
+    manager->addWidgetsToMainWindow(this);
+    //END WIDGET MANAGER EXAMPLE
+    //
   }
   
-void bringToFront()
-{
-//     this->show();
-//     this->showNormal();
-    this->raise();
-    this->activateWindow();
-}
-
-public slots:
-
-protected:
-void mousePressEvent(QMouseEvent *event)
-  {
-//     this->show();
-//     this->showNormal();
-    this->raise();
-    this->activateWindow();
-
-  }
-
-void mouseMoveEvent(QMouseEvent *event)
-  {
-//     this->show();
-//     this->showNormal();
-    this->raise();
-    this->activateWindow();
-
-  }
-void mouseReleaseEvent(QMouseEvent *event)
-  {
-//     this->show();
-//     this->showNormal();
-    this->raise();
-    this->activateWindow();
-
-  }
-  
-
 };
 
 #endif
