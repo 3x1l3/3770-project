f4bcb96d1d212ee10eefe998ce614a7c77b0d505
chadklassen1@gmail.com <chadklassen1@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-03-26 18:34:03 +0000

    Done the photos widget

--- widgets/photos/moc_photos.cpp
+++ widgets/photos/moc_photos.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'photos.h'
 **
-** Created: Thu Mar 24 22:54:42 2011
+** Created: Sat Mar 26 12:22:40 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,7 +23,7 @@ static const uint qt_meta_data_photos[] = {
        4,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -32,12 +32,15 @@ static const uint qt_meta_data_photos[] = {
 
  // slots: signature, parameters, type, tag, flags
        8,    7,    7,    7, 0x0a,
+      28,    7,    7,    7, 0x0a,
+      52,    7,    7,    7, 0x0a,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_photos[] = {
     "photos\0\0loadImages(QString)\0"
+    "showContextMenu(QPoint)\0changeDir()\0"
 };
 
 const QMetaObject photos::staticMetaObject = {
@@ -70,9 +73,11 @@ int photos::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
         case 0: loadImages((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 1: showContextMenu((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 2: changeDir(); break;
         default: ;
         }
-        _id -= 1;
+        _id -= 3;
     }
     return _id;
 }
