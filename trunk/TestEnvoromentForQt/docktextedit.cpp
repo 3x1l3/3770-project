efce39179fd876eada3dc6be65fda0158d0e7494
jay.racine@gmail.com <jay.racine@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-04-01 04:35:55 +0000

    ...once I add the actual file. lol

---
+++ trunk/TestEnvoromentForQt/docktextedit.cpp
@@ -0,0 +1,69 @@
+#include "docktextedit.h"
+
+
+DockTextEdit::DockTextEdit(QWidget* parent): QTextEdit(parent)
+{
+  isInvisible = false;
+  setMouseTracking(true);
+  fullArea = new QRegion(0, 0, width(), height(), QRegion::Rectangle);
+  noArea = new QRegion(15,15,50,50, QRegion::Rectangle);
+
+  
+  
+}
+
+
+DockTextEdit::~DockTextEdit()
+{
+
+}
+
+
+void DockTextEdit::leaveEvent(QEvent* event)
+{
+    QTextEdit::leaveEvent(event);
+    setMask(*fullArea);
+}
+
+void DockTextEdit::mouseMoveEvent(QMouseEvent* event)
+{ 
+    if(isInvisible)
+    {
+      event->ignore();
+    }
+  
+    QTextEdit::mouseMoveEvent(event);
+    
+    x = event->x();
+    y = event->y();
+    update();
+}
+
+
+void DockTextEdit::paintEvent(QPaintEvent* event)
+{
+    QTextEdit::paintEvent(event);
+    
+    QPainter painter(this);
+    
+    QPolygon myPolygon = QPolygon::QPolygon(QRect(0,0,width(), height()));
+    QPolygon mousePolygon = QPolygon::QPolygon(QRect(x, y, 3, 3));
+    
+    myPolygon = myPolygon.subtracted(mousePolygon);
+  //   cout << x << " " << y << " " << isInvisible << " " << true << " " << underMouse() <<  endl;
+    *noArea = QRegion(myPolygon,Qt::OddEvenFill );
+    *fullArea = QRegion(QRect(0,0,width(),height()));
+    
+    if(isInvisible == true && underMouse() == true)
+    {
+  //     cout << "\nIS UNDER MOUSE\n";
+	  setMask(*noArea);
+    }
+    
+}
+
+void DockTextEdit::setInvisible(bool setter)
+{
+  isInvisible = setter;
+}
+
