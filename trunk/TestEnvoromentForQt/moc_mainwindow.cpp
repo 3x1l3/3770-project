2d1b1285481923189b19c4182a7f72c00cbbc7d6
peoples1988@gmail.com <peoples1988@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-04-01 02:11:03 +0000

    changed some stuff trying to get mouse tracking click trhough more accurate.  also took out some redudnat code, and addde other redundant code.

--- trunk/TestEnvoromentForQt/moc_mainwindow.cpp
+++ trunk/TestEnvoromentForQt/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Wed Mar 30 21:59:50 2011
+** Created: Thu Mar 31 19:12:42 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.0)
 **
 ** WARNING! All changes made in this file will be lost!
