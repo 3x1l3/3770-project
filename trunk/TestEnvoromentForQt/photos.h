358a2ad7b726040278b5c13e0023d96ec235c367
tomcatadam@gmail.com <tomcatadam@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-04-01 04:42:49 +0000

    added photos test files

---
+++ trunk/TestEnvoromentForQt/photos.h
@@ -0,0 +1,62 @@
+#ifndef PHOTOS_H
+#define PHOTOS_H
+
+#include <QtGui/QWidget>
+#include <QLabel>
+#include <QBitmap>
+#include <QString>
+#include <QFileDialog>
+#include <QStringList>
+#include <iostream>
+#include <QDir>
+#include <QTimerEvent>
+#include <QMouseEvent>
+#include <QMenu>
+#include <QAction>
+#include <QMessageBox>
+
+class photos : public DockWidget
+{
+    Q_OBJECT
+
+public slots:
+    void loadImages(QString);
+    void showContextMenu(const QPoint&);
+    void changeDir();
+
+public:
+    QStringList files;
+    int currentfile;
+
+    photos(QWidget *parent = 0);
+    ~photos();
+    QLabel *label;
+    QPixmap *image;
+    QFileDialog *imageDialog;
+    QDir *directoryAccess;
+    QStringList filter;
+    QTimer *timer;
+    QString directory;
+    QMenu *context;
+    QMessageBox *nofiles;
+
+    QAction *changedirAct;
+    
+    QRegion *fullArea;
+    QRegion *noArea;
+    
+protected:
+    void paintEvent(QPaintEvent*);
+    void mouseMoveEvent(QMouseEvent*);
+
+
+private:
+    void timerEvent(QTimerEvent * event);
+
+    bool isInvisible;
+    int x;
+    int y;
+
+};
+
+#endif // PHOTOS_H
