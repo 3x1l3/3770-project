9cf7a72f658d1e1d9c7d73a9d28d9c40bbc3190e
chadklassen1@gmail.com <chadklassen1@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-04-02 19:54:09 +0000

    Cleanup on Main Window

--- TestEnvoromentForQt/moc_mainwindow.cpp
+++ TestEnvoromentForQt/moc_mainwindow.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Sat Apr 2 13:04:57 2011
-**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.0)
+** Created: Sat Apr 2 19:49:13 2011
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -11,7 +11,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 62
-#error "This file was generated using the moc from 4.7.0. It"
+#error "This file was generated using the moc from 4.6.2. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,33 +20,30 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_MainWindow[] = {
 
  // content:
-       5,       // revision
+       4,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       2,       // signalCount
+       1,       // signalCount
 
  // signals: signature, parameters, type, tag, flags
-      12,   11,   11,   11, 0x05,
-      43,   39,   11,   11, 0x05,
+      16,   12,   11,   11, 0x05,
 
  // slots: signature, parameters, type, tag, flags
-      67,   11,   11,   11, 0x0a,
-      76,   11,   11,   11, 0x0a,
-      85,   11,   11,   11, 0x08,
-     100,   11,   11,   11, 0x08,
+      40,   11,   11,   11, 0x0a,
+      49,   11,   11,   11, 0x0a,
+      58,   11,   11,   11, 0x08,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_MainWindow[] = {
-    "MainWindow\0\0setTransparencyOnWidgets()\0"
-    "x,y\0sendOutMouseXY(int,int)\0myhide()\0"
-    "myshow()\0showPassword()\0setTransparency()\0"
+    "MainWindow\0\0x,y\0sendOutMouseXY(int,int)\0"
+    "myhide()\0myshow()\0setTransparency()\0"
 };
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -78,29 +75,21 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
         switch (_id) {
-        case 0: setTransparencyOnWidgets(); break;
-        case 1: sendOutMouseXY((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
-        case 2: myhide(); break;
-        case 3: myshow(); break;
-        case 4: showPassword(); break;
-        case 5: setTransparency(); break;
+        case 0: sendOutMouseXY((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 1: myhide(); break;
+        case 2: myshow(); break;
+        case 3: setTransparency(); break;
         default: ;
         }
-        _id -= 6;
+        _id -= 4;
     }
     return _id;
 }
 
 // SIGNAL 0
-void MainWindow::setTransparencyOnWidgets()
-{
-    QMetaObject::activate(this, &staticMetaObject, 0, 0);
-}
-
-// SIGNAL 1
 void MainWindow::sendOutMouseXY(int _t1, int _t2)
 {
     void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
-    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 QT_END_MOC_NAMESPACE
