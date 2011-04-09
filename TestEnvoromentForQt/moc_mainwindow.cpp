c1546aa0dfa39134515b74b94be42cacb5d94156
peoples1988@gmail.com <peoples1988@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-04-09 18:39:17 +0000

    added a test case file. added three test cases in

--- TestEnvoromentForQt/moc_mainwindow.cpp
+++ TestEnvoromentForQt/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Thu Apr 7 18:30:29 2011
+** Created: Fri Apr 8 11:09:20 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.0)
 **
 ** WARNING! All changes made in this file will be lost!
