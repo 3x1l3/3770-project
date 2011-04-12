0c6df71271328c62e5083ad193aa2c0c13d39d31
peoples1988@gmail.com <peoples1988@gmail.com@93f95f75-1dcb-79a3-4bd3-c58e7eb24477>
2011-04-12 01:17:32 +0000

    added in a title label on the controller widget.  this is so the users will know what  the controller widget is for

--- trunk/TestEnvoromentForQt/moc_passwordedit.cpp
+++ trunk/TestEnvoromentForQt/moc_passwordedit.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'passwordedit.h'
 **
-** Created: Sat Apr 9 14:53:07 2011
+** Created: Mon Apr 11 18:26:35 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.0)
 **
 ** WARNING! All changes made in this file will be lost!
